2b7faef6402ed87d0ce9294cccdac36604521fff
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-06-11 16:53:48 -0300

    Mensajes de cargando y error

--- src/Platos.js
+++ src/Platos.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import ListadoDePlatos from "./ListadoDePlatos";
 
 const Platos = () => {
-    const [platos, setPlatos] = useState(null)
+    const [platos, setPlatos] = useState(null);
+    const [cargando, setCargando] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("https://il-piatto-api.herokuapp.com/platos")
@@ -10,12 +12,26 @@ const Platos = () => {
                 return response.json()
             })
             .then((data) => {
-                setPlatos(data);
+                if(data.codigo===200){
+                    setError(null)
+                    setPlatos(data);
+                }
+                else{
+                    setPlatos(null);
+                    setError(data.error)
+                }
+                setCargando(false);
+            })
+            .catch(error => {
+                setCargando(false);
+                setError(error.message)
             })
     }, [])
 
     return ( 
         <div className="platos">
+            {error && <div className="mensaje"> {error} </div>}
+            {cargando && <div className="mensaje">Cargando...</div>}
             {platos && <ListadoDePlatos platos={platos.plato}/>}
         </div>
     );
