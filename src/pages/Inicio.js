37bb135bb8de7b1b30b9a0b9651784e7f7e53e80
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-06-13 22:03:38 -0300

    Agregado texto en la pagina de inicio y galeria

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,7 +1,13 @@
+import Galeria from '../components/Galeria';
+
 const Inicio = () => {
     return ( 
         <div className="inicio">
-            <h1>Bienvenido</h1>
+            <h3>Desde nuestra apertura en 1996, ofrecemos una comida auténtica basada en la cocina italiana tradicional. La calidad de nuestros productos se respalda por el uso de horno de leña para la elaboración de las pizzas y el pan. Pero si en algo nos caracterizamos es por nuestra preocupación por las personas con algún tipo de intolerancia alimenticia. Destacan los menús especiales aptos para celíacos, intolerantes a la lactosa o gente que esté realizando algún tipo de dieta.
+            Una mesa repleta de productos tradicionales que podrás acompañar gracias a una extensa carta de vinos que incluye ejemplares de origen italiano y español.
+            </h3>
+            <br/>
+            <Galeria/>
         </div>   
     );
 }
